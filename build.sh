cc97c37046c7dc45d91053a8f085b62c0d88d34c
Vinz2008 <vincentbidarddelanoe@gmail.com>
2024-10-19 13:42:14 +0200

    make build script cleaner

--- build.sh
+++ build.sh
@@ -1,2 +1,15 @@
+#/bin/bash
 # call this from the build dir
-cmake -S ../llvm -B . -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/home/vincent/llvm_static/sysroot/usr -DCMAKE_SKIP_RPATH=ON -DLLVM_ENABLE_RTTI=ON -DLLVM_BINUTILS_INCDIR=/usr/include -DBUILD_SHARED_LIBS=false -DLIBCLANG_BUILD_STATIC=ON -DLLVM_ENABLE_BINDINGS=OFF -DLLVM_ENABLE_PROJECTS="clang;lld"
+cmake -S ../llvm -B . \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX="$HOME/llvm_static/sysroot/usr" \
+    -DCMAKE_SKIP_RPATH=ON \
+    -DLLVM_ENABLE_RTTI=ON \
+    -DLLVM_BINUTILS_INCDIR=/usr/include \
+    -DBUILD_SHARED_LIBS=false \
+    -DLIBCLANG_BUILD_STATIC=ON \
+    -DLLVM_ENABLE_BINDINGS=OFF \
+    -DLLVM_USE_LINKER=mold \
+    -DLLVM_CCACHE_BUILD=true \
+    -DLLVM_ENABLE_PROJECTS="clang;lld" \
+     -G Ninja
